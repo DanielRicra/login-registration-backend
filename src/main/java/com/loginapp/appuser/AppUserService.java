10cd191549e89083b4ce56baeabcae2145640636
TSdnl13 <daniel.int@outlook.com>
2022-04-03 23:04:59 -0500

    appuser and registration

--- src/main/java/com/loginapp/appuser/AppUserService.java
+++ src/main/java/com/loginapp/appuser/AppUserService.java
@@ -1,17 +1,31 @@
 package com.loginapp.appuser;
 
+import com.loginapp.email.EmailSender;
+import com.loginapp.registration.RegistrationService;
+import com.loginapp.registration.token.ConfirmationToken;
+import com.loginapp.registration.token.ConfirmationTokenRepository;
+import com.loginapp.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
     private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
+
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) {
@@ -19,4 +33,34 @@ public class AppUserService implements UserDetailsService {
                 .orElseThrow(() ->
                         new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
+
+    public String signUpUser(AppUser appUser) {
+        Optional<AppUser> existUser = appUserRepository.findByEmail(appUser.getEmail());
+        if (existUser.isPresent()) {
+            throw new IllegalStateException("Email already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+        appUserRepository.save(appUser);
+
+        String token = UUID.randomUUID().toString();
+
+        ConfirmationToken confirmationToken = new ConfirmationToken(
+                token,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(15),
+                appUser
+        );
+
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        // TODO: Send email
+        return token;
+    }
+
+    public int enableAppUser(String email) {
+        return appUserRepository.enableAppUser(email);
+    }
+
 }
---
+++ src/main/java/com/loginapp/registration/token/ConfirmationTokenRepository.java
@@ -0,0 +1,29 @@
+package com.loginapp.registration.token;
+
+import com.loginapp.appuser.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+    Optional<ConfirmationToken> findByAppUser(AppUser appUser);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE ConfirmationToken c SET c.confirmedAt = ?2 WHERE c.token = ?1")
+    int updateConfirmedAt(String token, LocalDateTime now);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE ConfirmationToken c SET c.expiresAt = ?2 WHERE c.token = ?1")
+    int updateExpiresAt(String token, LocalDateTime plusMinutes);
+}
